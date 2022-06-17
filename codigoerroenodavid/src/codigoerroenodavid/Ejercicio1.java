c7b4ebce28975dc7916e74e73a7ffcaecc7a3c99
David Carrasco <yendaadan@gmail.com>
2022-06-17 21:08:14 +0200

    ejercicios con errores

---
+++ codigoerroenodavid/src/codigoerroenodavid/Ejercicio1.java
@@ -0,0 +1,11 @@
+package codigoerroenodavid;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		//corrige falta el punto y coma
+		System.out.println()
+
+	}
+
+}
---
+++ codigoerroenodavid/src/codigoerroenodavid/Ejercicio2.java
@@ -0,0 +1,11 @@
+package codigoerroenodavid;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		//corrige los parentesis
+		System.out.println;
+
+	}
+
+}
